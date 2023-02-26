3da8d5a3b28fc8ec32fb05552756ba780539faf7
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-02-27 01:12:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
                     commands : {
                     car : 0,
-                    moto : 7,
+                    moto :,
                     bicycle : 0,
                 },
                 price :{
